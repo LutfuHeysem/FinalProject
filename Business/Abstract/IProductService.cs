337a6a1399e9da3684b721b4014501356d9bd0f2
Lütfü Heysem <heysem1104@gmail.com>
2021-02-03 23:57:04 +0300

    03/02/2021

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -8,5 +8,7 @@ namespace Business.Abstract
     public interface IProductService
     {
         List<Product> GetAll();
+        List<Product> GetAllByCaategoryId(int id);
+        List<Product> GetByUnitPrice(decimal min, decimal max);
     }
 }
